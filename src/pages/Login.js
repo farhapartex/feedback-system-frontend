5462ac64eaffa3e9b331e2d7a8e9b10aae8abb90
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-09 19:02:36 +0600

    text field added with custom component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,16 @@
 import React, {Component} from "react";
+import TextFieldGroup from "../component/common/textFieldGroup";
 
 class Login extends Component{
     render(){
         return (
-            <div>Login Page</div>
+            <div id="loginPage">
+                <div id="login-block">
+                    <h4 className="text-center mb-3">Login</h4>
+                    <TextFieldGroup type="text" label="Email" className="form-control" />
+                    <TextFieldGroup type="password" label="Password" className="form-control" />
+                </div>
+            </div>
         )
     }
 }
