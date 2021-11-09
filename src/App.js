8c2463a90b1f4af9516e8c23d07d2dcd178a926b
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-09 16:58:05 +0600

    created RootPage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import './App.css';
 import Login from "./pages/Login";
+import RootPage from "./pages/RootPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<RootPage/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
     </div>
---
+++ src/pages/RootPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class RootPage extends Component{
+    render(){
+        return (
+            <div>Root Page</div>
+        )
+    }
+}
+
+export default RootPage;
