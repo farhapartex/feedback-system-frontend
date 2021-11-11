78f9697651e66df10c8c6d742b02ba0ad905a1bb
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-11 16:05:35 +0600

    form validation added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import {Routes, Route} from "react-router-dom";
 import './App.css';
 import Login from "./pages/Login";
@@ -7,7 +6,6 @@ import RootPage from "./pages/RootPage";
 
 function App() {
   const user = localStorage.getItem("user");
-  console.log(user)
 
   if(user){
     return (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,31 @@
 import React, { useState }  from "react";
 import { useDispatch } from "react-redux";
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Alert } from 'react-bootstrap';
 import { login } from "../features/userSlice";
 
 const Login = ()=>{
     
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isShowErrorMsg, setIsShowErrorMsg] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const dispatch = useDispatch();
 
     const handleLogin = () => {
+
+        const data = [
+            {field: "Email", value: email},
+            {field: "Password", value: password}
+        ]
+
+        const response = formValidation(data);
+        if (response){
+            setIsShowErrorMsg(true);
+            setErrorMessage(response);
+
+            return;
+        }
         const requestBody = {
             email: email,
             password: password,
@@ -18,6 +33,28 @@ const Login = ()=>{
         }
 
         dispatch(login(requestBody));
+    }
+
+    const validateEmail = (email) =>{
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const formValidation = (fieldList)=>{
+        const errorList = []
+        
+        fieldList.forEach((item, index) =>{
+            console.log(item)
+            if(! item.value || item.length === 0){
+                errorList.push(item.field + " is missing")
+            }
+            else if(item.field == "Email" && !validateEmail(item.value)){
+                errorList.push(item.field + " is not valid.")
+            }
+        });
+
+        return errorList.join(", ")
+
     }
 
 
@@ -34,6 +71,7 @@ const Login = ()=>{
                     <Form.Control type="password" value={password} onChange={(e)=> setPassword(e.target.value)}></Form.Control>
                 </Form.Group>
                 <Button className="mt-3 w-100" varient="primary" onClick={handleLogin}>Login</Button>
+                { isShowErrorMsg && <Alert className="mt-3" variant="danger">{errorMessage}</Alert>}
             </div>
         </div>
     );
