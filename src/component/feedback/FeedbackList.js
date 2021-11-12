54ed8ba5e1c51921f004bc81c2fdb09b763e090d
Md Nazmul Hasan <farhapartex@gmail.com>
2021-11-13 02:50:19 +0600

    feedback dummy data added

--- src/component/feedback/FeedbackList.js
+++ src/component/feedback/FeedbackList.js
@@ -3,68 +3,12 @@ import {ListGroup, Table, Container} from "react-bootstrap";
 import FeedbackCard from "./FeedbackCard";
 import Thead from "../common/table/Thead";
 import Trow from "../common/table/Trow";
-
-
-const  dummyFeedbackData = [
-    {
-        title: "Where does it come from?",
-        shortDescription: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using",
-        supporter: "259",
-        reviewer: "Nazmul Hasan"
-    },
-    {
-        title: "Where can I get some?",
-        shortDescription: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using",
-        supporter: "164",
-        reviewer: null
-    },
-    {
-        title: "Where does it come from?",
-        shortDescription: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using",
-        supporter: "114",
-        reviewer: null
-    }
-
-];
+import { feedbackDataList } from "../../dummyData/feedbackData";
 
 class FeedbackList extends Component{
 
     tableColums = ["#", "Department", "Topic", "Status", "Feedbacks", "Action"]
-    tableRows = [
-        {
-            "id": 1,
-            "department": "Engineering",
-            "topic": "The use of negative feedback in amplifier and process control",
-            "status": "Running",
-            "feedbacks": 124,
-            "action": "View"
-        },
-        {
-            "id": 2,
-            "department": "Marketing",
-            "topic": "The use of negative feedback in amplifier and process control",
-            "status": "Resolved",
-            "feedbacks": 268,
-            "action": "View"
-        },
-        {
-            "id": 3,
-            "department": "Engineering",
-            "topic": "The use of negative feedback in amplifier and process control",
-            "status": "Resolved",
-            "feedbacks": 322,
-            "action": "View"
-        }
-    ]
-
-    renderListItem = () =>{
-        let dataList = []
-        dummyFeedbackData.forEach((item, index) => {
-            dataList.push(<ListGroup.Item key={index}><FeedbackCard title={item.title} shortDescription={item.shortDescription} supporter={item.supporter} reviewer={item.reviewer}/></ListGroup.Item>)
-        })
-
-        return dataList;
-    }
+    tableRows = feedbackDataList;
 
     renderRows = ()=>{
         let rowList = [];
---
+++ src/dummyData/feedbackData.js
@@ -0,0 +1,42 @@
+export const feedbackDataList = [
+    {
+        "id": 1,
+        "department": "Engineering",
+        "topic": "The use of negative feedback in amplifier and process control",
+        "status": "Running",
+        "feedbacks": 124,
+        "action": "View"
+    },
+    {
+        "id": 2,
+        "department": "Marketing",
+        "topic": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "status": "Resolved",
+        "feedbacks": 268,
+        "action": "View"
+    },
+    {
+        "id": 3,
+        "department": "Engineering",
+        "topic": "There are many variations of passages of Lorem Ipsum available",
+        "status": "Resolved",
+        "feedbacks": 322,
+        "action": "View"
+    },
+    {
+        "id": 4,
+        "department": "HR & Admin",
+        "topic": "Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum'",
+        "status": "Running",
+        "feedbacks": 119,
+        "action": "View"
+    },
+    {
+        "id": 5,
+        "department": "HR & Admin",
+        "topic": "The use of negative feedback in amplifier and process control",
+        "status": "Running",
+        "feedbacks": 112,
+        "action": "View"
+    }
+]
